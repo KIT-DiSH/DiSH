8a34e8e763acb30aef5aa2f1f6df6cda420c45e2
shou-watanabe <watanabeshou1520@gmail.com>
2021-08-16 01:38:48 +0900

    [add]色の定数

---
+++ lib/configs/constant_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColor {
+  static const kPrimaryTextColor = const Color(0x333333);
+  static const kStarYellowColor = const Color(0xFFD702);
+  static const kPinkColor = const Color(0xFF537C);
+}
