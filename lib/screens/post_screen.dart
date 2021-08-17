e2761f0cf5036e744a7e32451c882b14d6cede65
shou-watanabe <watanabeshou1520@gmail.com>
2021-08-17 20:08:00 +0900

    [update]#3 フォーム外タップでキーボードをしまう

--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -11,57 +11,59 @@ class PostScreen extends StatelessWidget {
     final _hintText = "投稿文を書く";
     final _restaurantName = "";
 
-    return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.arrow_back_ios),
-        title: Text(_titleText),
-        centerTitle: true,
-        elevation: 0,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.check,
-              color: AppColor.kPinkColor,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.arrow_back_ios),
+          title: Text(_titleText),
+          centerTitle: true,
+          elevation: 0,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.check,
+                color: AppColor.kPinkColor,
+              ),
+              onPressed: () {},
             ),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: SafeArea(
-        child: Form(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Column(
-                  children: <Widget>[
-                    const SizedBox(height: 8),
-                    Container(
-                      width: double.infinity,
-                      child: Text(
-                        _restaurantName != "" ? _restaurantName : "店名",
-                        textAlign: TextAlign.left,
-                      ),
-                    ),
-                    Container(
-                      child: TextField(
-                        style: TextStyle(
-                          fontSize: 12.0,
-                          color: AppColor.kPrimaryTextColor,
-                        ),
-                        keyboardType: TextInputType.multiline,
-                        maxLines: null,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: _hintText,
+          ],
+        ),
+        body: SafeArea(
+          child: Form(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(height: 8),
+                      Container(
+                        width: double.infinity,
+                        child: Text(
+                          _restaurantName != "" ? _restaurantName : "店名",
+                          textAlign: TextAlign.left,
                         ),
                       ),
-                      height: 250,
-                    ),
-                  ],
-                ),
-              )
-            ],
+                      Container(
+                        child: TextField(
+                          style: TextStyle(
+                            color: AppColor.kPrimaryTextColor,
+                          ),
+                          keyboardType: TextInputType.multiline,
+                          maxLines: null,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: _hintText,
+                            contentPadding: EdgeInsets.zero,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
