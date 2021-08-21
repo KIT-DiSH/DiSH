2c5c70fef6de15749c8d238606f7ae9989585d9d
tomo-taku <tmrtkm1227tksx@gmail.com>
2021-08-21 15:23:32 +0900

    [add]#10 フォロー・フォロワー一覧画面の作成

---
+++ lib/screens/follow_list_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import 'package:dish/configs/constant_colors.dart';
+import 'package:dish/widgets/follow_follower_list_screen/user_card.dart';
+
+class FollowListScreen extends StatefulWidget {
+  @override
+  _FollowListScreenState createState() => _FollowListScreenState();
+}
+
+class _FollowListScreenState extends State<FollowListScreen> {
+  final _title = "フォロー中";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: Container(
+        child: ListView(
+          children: [
+            UserCard(),
+            Divider(
+              height: 1,
+              thickness: 1,
+              color: AppColor.kDefaultBorderColor.withOpacity(0.75),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 70,
+        decoration: BoxDecoration(color: Colors.black12),
+        child: Center(child: Text("footer")),
+      ),
+    );
+  }
+
+  PreferredSize _buildAppBar() {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(50.0),
+      child: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 1.0,
+        leading: Icon(
+          Icons.arrow_back_ios,
+          color: Colors.black,
+        ),
+        title: Text(
+          _title,
+          style: TextStyle(
+            color: AppColor.kPrimaryTextColor,
+            fontSize: 16,
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Icon(
+              Icons.more_horiz,
+              color: AppColor.kPrimaryTextColor,
+            ),
+          ),
+          SizedBox(width: 12),
+        ],
+      ),
+    );
+  }
+}
