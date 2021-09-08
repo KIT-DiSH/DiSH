0b8634e1322c19e88c784de7b69943cac55fe631
shou-watanabe <watanabeshou1520@gmail.com>
2021-09-08 18:16:04 +0900

    [update]#3 TextButtonに変更

--- lib/widgets/post_screen/hashtag_list.dart
+++ lib/widgets/post_screen/hashtag_list.dart
@@ -36,35 +36,53 @@ class _HashtagListState extends State<HashtagList> {
               itemCount: dummyHashtags.length,
               separatorBuilder: (_, __) => const SizedBox(width: 8),
               itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTapDown: (_) {
-                    setState(() {
-                      _selectedIndex = index;
-                    });
+                return TextButton(
+                  onPressed: () {
                     widget.emitHashtag(dummyHashtags[index]);
                   },
-                  onTapUp: (_) async {
-                    // Why: 離すのが早すぎると色が変わらないので.1秒遅らせる
-                    await new Future.delayed(
-                      new Duration(milliseconds: 100),
-                    );
-                    setState(() {
-                      _selectedIndex = -1;
-                    });
-                  },
-                  onTapCancel: () {
-                    setState(() {
-                      _selectedIndex = -1;
-                    });
-                  },
-                  child: Chip(
-                    label: Text(dummyHashtags[index]),
-                    backgroundColor: _selectedIndex == index
-                        ? AppColor.kPinkColor.withOpacity(0.7)
-                        : Colors.grey.shade300,
-                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                  style: ElevatedButton.styleFrom(
+                    primary: AppColor.kMaterialGrayColor,
+                    shape: const StadiumBorder(),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Text(
+                      dummyHashtags[index],
+                      style: TextStyle(
+                        color: AppColor.kPrimaryTextColor,
+                      ),
+                    ),
                   ),
                 );
+                // return GestureDetector(
+                //   onTapDown: (_) {
+                //     setState(() {
+                //       _selectedIndex = index;
+                //     });
+                //     widget.emitHashtag(dummyHashtags[index]);
+                //   },
+                //   onTapUp: (_) async {
+                //     // Why: 離すのが早すぎると色が変わらないので.1秒遅らせる
+                //     await new Future.delayed(
+                //       new Duration(milliseconds: 100),
+                //     );
+                //     setState(() {
+                //       _selectedIndex = -1;
+                //     });
+                //   },
+                //   onTapCancel: () {
+                //     setState(() {
+                //       _selectedIndex = -1;
+                //     });
+                //   },
+                //   child: Chip(
+                //     label: Text(dummyHashtags[index]),
+                //     backgroundColor: _selectedIndex == index
+                //         ? AppColor.kPinkColor.withOpacity(0.7)
+                //         : Colors.grey.shade300,
+                //     padding: const EdgeInsets.symmetric(horizontal: 12),
+                //   ),
+                // );
               },
               scrollDirection: Axis.horizontal,
             ),
