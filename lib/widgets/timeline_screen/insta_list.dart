ebe2c7bab439e96bf440ec7185a47acaae23aac9
FujimuraKaito <kaito.bakuman@gmail.com>
2021-09-09 09:49:36 +0900

    [refactor] refactor code

--- lib/widgets/timeline_screen/insta_list.dart
+++ lib/widgets/timeline_screen/insta_list.dart
@@ -1,119 +1,7 @@
 import 'package:dish/configs/constant_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-class User {
-  String id;
-  String name;
-  String imageUrl;
-
-  User({
-    required this.id,
-    required this.name,
-    required this.imageUrl,
-  });
-}
-
-class Comment {
-  User user;
-  String text;
-
-  Comment({
-    required this.user,
-    required this.text,
-  });
-}
-
-class PostModel {
-  bool isTextExpanded;
-  bool isCommentExpanded;
-  int star;
-  String image;
-  User user;
-  String shop;
-  String discription;
-  String tags;
-  String map;
-  List<Comment> comments;
-
-  PostModel({
-    this.isTextExpanded: false,
-    this.isCommentExpanded: false,
-    required this.star,
-    required this.image,
-    required this.user,
-    required this.shop,
-    required this.discription,
-    required this.tags,
-    required this.map,
-    required this.comments,
-  });
-}
-
-List<PostModel> posts = [
-  PostModel(
-    star: 3,
-    image: 'https://picsum.photos/600',
-    user: User(
-      id: 'aaa',
-      name: 'kaito',
-      imageUrl: 'https://picsum.photos/40',
-    ),
-    shop: '馬肉専門店　うまうま王国',
-    discription: 'おいしそうおいしそうおいしそうおいしそうおいしそうおいしそうおいしそうおいしそうおいしそうおいしそうおいしそうおいしそう',
-    tags: '#馬うますぎて草 #有馬記念',
-    map: 'https://amazom.com',
-    comments: ([
-      Comment(
-        user: User(
-          id: '',
-          name: 'fujimura',
-          imageUrl: 'https://picsum.photos/42',
-        ),
-        text: '美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった',
-      ),
-      Comment(
-        user: User(
-          id: '',
-          name: 'fujimura',
-          imageUrl: 'https://picsum.photos/44',
-        ),
-        text: '美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった',
-      ),
-    ]),
-  ),
-  PostModel(
-    star: 4,
-    image: 'https://picsum.photos/601',
-    user: User(
-      id: 'aaa',
-      name: 'fuji',
-      imageUrl: 'https://picsum.photos/41',
-    ),
-    shop: '羊肉専門店　メーメー王国',
-    discription: 'おいしそうおいしそうおいしそうおいしそうおいしそうおいしそうおいしそうおいしそうおいしそうおいしそうおいしそうおいしそう',
-    tags: '#羊うますぎて草 #有馬記念',
-    map: 'https://amazom.com',
-    comments: ([
-      Comment(
-        user: User(
-          id: '',
-          name: 'kaitoooo',
-          imageUrl: 'https://picsum.photos/43',
-        ),
-        text: '美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった',
-      ),
-      Comment(
-        user: User(
-          id: '',
-          name: 'fujiiiiii',
-          imageUrl: 'https://picsum.photos/45',
-        ),
-        text: '美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった美味しかった',
-      ),
-    ]),
-  ),
-];
+import 'package:dish/models/PostModel.dart';
 
 class InstaList extends StatefulWidget {
   @override
@@ -123,7 +11,6 @@ class InstaList extends StatefulWidget {
 class _InstaListState extends State<InstaList> {
   @override
   Widget build(BuildContext context) {
-    // var devicesize = MediaQuery.of(context).size;
     const IconData star = IconData(0xe5f9, fontFamily: 'MaterialIcons');
 
     return new ListView.builder(
@@ -146,74 +33,68 @@ class _InstaListState extends State<InstaList> {
           Padding(
             padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
-                  children: [
-                    new Container(
-                      height: 40,
-                      width: 40,
-                      decoration: new BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: new DecorationImage(
-                          fit: BoxFit.fill,
-                          image: new NetworkImage(
-                            posts[index].user.imageUrl,
-                          ),
-                        ),
+                Container(
+                  height: 40,
+                  width: 40,
+                  decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: new DecorationImage(
+                      fit: BoxFit.fill,
+                      image: new NetworkImage(
+                        posts[index].user.imageUrl,
                       ),
                     ),
-                    new SizedBox(width: 10),
-                    Column(
+                  ),
+                ),
+                new SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    new Text(
+                      posts[index].user.name,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.start,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        new Text(
-                          posts[index].user.name,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.start,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            for (int i = 0; i < posts[index].star; i++)
-                              new Icon(
-                                star,
+                        for (int i = 0; i < posts[index].star; i++)
+                          new Icon(
+                            star,
+                          ),
+                        for (int i = 0; i < 5 - posts[index].star; i++)
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              right: 4.0,
+                            ),
+                            child: SizedBox(
+                              child: new Icon(
+                                FontAwesomeIcons.star,
+                                size: 16,
                               ),
-                            for (int i = 0; i < 5 - posts[index].star; i++)
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                  right: 4.0,
-                                ),
-                                child: SizedBox(
-                                  child: new Icon(
-                                    FontAwesomeIcons.star,
-                                    size: 16,
-                                  ),
-                                ),
-                              ),
-                          ],
-                        ),
+                            ),
+                          ),
                       ],
                     ),
                   ],
                 ),
-                Row(
-                  children: [
-                    Directionality(
-                      textDirection: TextDirection.rtl,
-                      child: ElevatedButton.icon(
-                        label: const Text('マップで見る'),
-                        icon: const Icon(
-                          Icons.arrow_left,
-                          color: Colors.white,
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          primary: AppColor.kPinkColor,
-                          onPrimary: Colors.white,
-                        ),
-                        onPressed: () {},
-                      ),
+                Spacer(),
+                Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: ElevatedButton.icon(
+                    label: const Text('マップで見る'),
+                    icon: const Icon(
+                      Icons.arrow_left,
+                      color: Colors.white,
                     ),
-                  ],
+                    style: ElevatedButton.styleFrom(
+                      primary: AppColor.kPinkColor,
+                      onPrimary: Colors.white,
+                    ),
+                    onPressed: () {},
+                  ),
                 ),
               ],
             ),
@@ -225,18 +106,12 @@ class _InstaListState extends State<InstaList> {
               horizontal: 16,
               vertical: 8,
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  posts[index].shop,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 12,
-                  ),
-                ),
-                SizedBox(width: 10),
-              ],
+            child: Text(
+              posts[index].shop,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 12,
+              ),
             ),
           ),
 
@@ -270,17 +145,12 @@ class _InstaListState extends State<InstaList> {
               horizontal: 16,
               vertical: 8,
             ),
-            child: Row(
-              children: [
-                Text(
-                  posts[index].tags,
-                  style: TextStyle(
-                    fontSize: 10,
-                    color: Colors.orange,
-                  ),
-                ),
-                SizedBox(width: 10),
-              ],
+            child: Text(
+              posts[index].tags,
+              style: TextStyle(
+                fontSize: 10,
+                color: Colors.orange,
+              ),
             ),
           ),
 
