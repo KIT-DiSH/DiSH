72e7600d01f4a256436211eb446c289f11d0f89d
FujimuraKaito <kaito.bakuman@gmail.com>
2021-09-12 10:11:07 +0900

    [add] add favorite button

--- lib/widgets/timeline_screen/image_slider.dart
+++ lib/widgets/timeline_screen/image_slider.dart
@@ -9,6 +9,7 @@ class ImageSlider extends StatefulWidget {
 
 class _ImageSlideerState extends State<ImageSlider> {
   int activeIndex = 0;
+  bool favorite = false;
   final List<String> imgList = [
     'https://picsum.photos/600',
     'https://picsum.photos/601',
@@ -24,21 +25,59 @@ class _ImageSlideerState extends State<ImageSlider> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        CarouselSlider.builder(
-          options: CarouselOptions(
-            enableInfiniteScroll: false,
-            enlargeCenterPage: false,
-            height: _width,
-            viewportFraction: 1,
-            onPageChanged: (index, reason) => {
-              setState(() => activeIndex = index),
-            },
-          ),
-          itemCount: imgList.length,
-          itemBuilder: (context, index, realIndex) {
-            final url = imgList[index];
-            return buildImage(context, url, index);
-          },
+        Stack(
+          children: [
+            CarouselSlider.builder(
+              options: CarouselOptions(
+                enableInfiniteScroll: false,
+                enlargeCenterPage: false,
+                height: _width,
+                viewportFraction: 1,
+                onPageChanged: (index, reason) => {
+                  setState(() => activeIndex = index),
+                },
+              ),
+              itemCount: imgList.length,
+              itemBuilder: (context, index, realIndex) {
+                final url = imgList[index];
+                return buildImage(context, url, index);
+              },
+            ),
+            Positioned(
+              right: 20,
+              bottom: 20,
+              child: Stack(
+                children: [
+                  Positioned(
+                    right: 12,
+                    bottom: 5,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                      ),
+                      width: 40,
+                      height: 40,
+                    ),
+                  ),
+                  TextButton(
+                    child: Icon(
+                      favorite == true ? Icons.favorite : Icons.favorite_border,
+                      color: favorite == true ? Colors.red : Colors.black38,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        if (!favorite)
+                          favorite = true;
+                        else
+                          favorite = false;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
         const SizedBox(height: 16),
         buildIndicator(),
