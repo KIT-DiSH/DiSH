53fee1c6344f5416f72a14cffffbb30fa75105c4
shou-watanabe <watanabeshou1520@gmail.com>
2021-09-25 18:38:09 +0900

    [add]#29 Validationの実装

--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -18,8 +18,11 @@ class PostScreen extends StatefulWidget {
 
 class _PostScreenState extends State<PostScreen> {
   late RichTextController _postTextController;
-
-  String restaurantName = "";
+  late TextEditingController _restaurantNameController =
+      TextEditingController();
+  Color checkIconColor = Colors.blueGrey;
+  static final _formKey = GlobalKey<FormState>();
+  final _initRestaurantName = "場所";
 
   @override
   void initState() {
@@ -29,6 +32,7 @@ class _PostScreenState extends State<PostScreen> {
       },
       // onMatch: () {print("#だよ");},
     );
+    _restaurantNameController.text = _initRestaurantName;
     super.initState();
   }
 
@@ -37,10 +41,25 @@ class _PostScreenState extends State<PostScreen> {
     final _hintText = "投稿文を書く";
     final _maxLength = 250;
 
+    void updateCheckIconColor() {
+      setState(() {
+        if (_formKey.currentState!.validate()) {
+          checkIconColor = AppColor.kPinkColor;
+        } else {
+          checkIconColor = Colors.blueGrey;
+        }
+      });
+    }
+
     void changeResName(String name) {
       setState(() {
-        restaurantName = name;
+        if (name == "") {
+          _restaurantNameController.text = _initRestaurantName;
+        } else {
+          _restaurantNameController.text = name;
+        }
       });
+      updateCheckIconColor();
     }
 
     void addHashtag(String name) {
@@ -53,62 +72,84 @@ class _PostScreenState extends State<PostScreen> {
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
-        appBar: _buildAppBar(context),
+        appBar: _buildAppBar(context, _formKey, checkIconColor),
         body: SafeArea(
           child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Column(
-                    children: <Widget>[
-                      const SizedBox(height: 8),
-                      Container(
-                        width: double.infinity,
-                        child: Text(
-                          restaurantName != "" ? restaurantName : "店名",
-                          textAlign: TextAlign.left,
-                          style: Theme.of(context).textTheme.headline6,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      Container(
-                        height: 200,
-                        child: TextField(
-                          style: Theme.of(context).textTheme.bodyText2,
-                          keyboardType: TextInputType.multiline,
-                          maxLines: 100,
-                          controller: _postTextController,
-                          maxLength: _maxLength,
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: _hintText,
-                            contentPadding: EdgeInsets.zero,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      children: <Widget>[
+                        const SizedBox(height: 8),
+                        Container(
+                          width: double.infinity,
+                          child: TextFormField(
+                            readOnly: true,
+                            controller: _restaurantNameController,
+                            textAlign: TextAlign.left,
+                            style: Theme.of(context).textTheme.headline6,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                            ),
+                            validator: (value) {
+                              if (value == _initRestaurantName) {
+                                return "場所を選択してください";
+                              }
+                              return null;
+                            },
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 16),
-                    ],
+                        const SizedBox(height: 8),
+                        Container(
+                          height: 200,
+                          child: TextFormField(
+                            style: Theme.of(context).textTheme.bodyText2,
+                            keyboardType: TextInputType.multiline,
+                            maxLines: 100,
+                            controller: _postTextController,
+                            maxLength: _maxLength,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: _hintText,
+                              contentPadding: EdgeInsets.zero,
+                            ),
+                            onChanged: (_) {
+                              updateCheckIconColor();
+                            },
+                            validator: (value) {
+                              if (value?.length == 0) {
+                                return "投稿文を記入してください";
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                      ],
+                    ),
                   ),
-                ),
-                ImageList(),
-                const SizedBox(height: 8),
-                SimpleDivider(),
-                HashtagList(emitHashtag: addHashtag),
-                SimpleDivider(),
-                PlaceList(emitRestaurantName: changeResName),
-                SimpleDivider(),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Column(
-                    children: <Widget>[
-                      StarReview(sectionName: "料理"),
-                      StarReview(sectionName: "雰囲気"),
-                      StarReview(sectionName: "コスパ"),
-                    ],
+                  ImageList(),
+                  const SizedBox(height: 8),
+                  SimpleDivider(),
+                  HashtagList(emitHashtag: addHashtag),
+                  SimpleDivider(),
+                  PlaceList(emitRestaurantName: changeResName),
+                  SimpleDivider(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      children: <Widget>[
+                        StarReview(sectionName: "料理"),
+                        StarReview(sectionName: "雰囲気"),
+                        StarReview(sectionName: "コスパ"),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
@@ -116,7 +157,8 @@ class _PostScreenState extends State<PostScreen> {
     );
   }
 
-  AppBar _buildAppBar(BuildContext context) {
+  AppBar _buildAppBar(BuildContext context, GlobalKey<FormState> _formKye,
+      Color checkIconColor) {
     final _titleText = "新規投稿";
 
     return AppBar(
@@ -139,9 +181,12 @@ class _PostScreenState extends State<PostScreen> {
         IconButton(
           icon: Icon(
             Icons.check,
-            color: AppColor.kPinkColor,
+            color: checkIconColor,
           ),
           onPressed: () {
+            if (_formKye.currentState!.validate()) {
+              Navigator.pop(context);
+            }
             // ここで投稿作成の処理
           },
         ),
