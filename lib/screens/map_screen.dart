71186ae107d10e9705a47857abd21b8d3bae1ac3
shou-watanabe <watanabeshou1520@gmail.com>
2021-09-25 21:08:52 +0900

    [add]#29 地図の微調整

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -41,52 +41,49 @@ class MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            GoogleMap(
-              mapType: MapType.hybrid,
-              initialCameraPosition: currentPosition,
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
-              onTap: (LatLng latLng) {
-                setState(() {
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.hybrid,
+            initialCameraPosition: currentPosition,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+            onTap: (LatLng latLng) {
+              setState(() {
+                pinLatLng = latLng;
+              });
+            },
+            markers: <Marker>[
+              Marker(
+                markerId: MarkerId("0"),
+                position: pinLatLng,
+                draggable: true,
+                onDragEnd: (LatLng latLng) {
                   pinLatLng = latLng;
-                });
-              },
-              markers: <Marker>[
-                Marker(
-                  markerId: MarkerId("0"),
-                  position: pinLatLng,
-                  draggable: true,
-                  onDragEnd: (LatLng latLng) {
-                    pinLatLng = latLng;
-                  },
+                },
+              ),
+            ].toSet(),
+            myLocationEnabled: true,
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FloatingActionButton(
+                heroTag: "hero2",
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.arrow_back_ios_new,
+                  color: Colors.black,
                 ),
-              ].toSet(),
-              myLocationEnabled: true,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
             ),
-            IconButton(
-              icon: const Icon(Icons.arrow_back_ios),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 16),
-        child: FloatingActionButton(
-          onPressed: () {
-            // ピンの座標からお店を取得する処理
-            Navigator.pop(context);
-          },
-          child: Icon(Icons.check),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
