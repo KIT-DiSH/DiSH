0b47f528286ca6d45f7424d76fc730e7807f771d
FujimuraKaito <kaito.bakuman@gmail.com>
2021-09-27 19:48:25 +0900

    [add] add login methods

--- lib/screens/select_signin_signup_screen.dart
+++ lib/screens/select_signin_signup_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 import 'package:dish/screens/signup_screen.dart';
 import 'package:dish/screens/signin_screen.dart';
@@ -7,7 +10,16 @@ import 'package:dish/configs/constant_colors.dart';
 import 'package:dish/widgets/common/divider_with_label.dart';
 import 'package:dish/widgets/signin_signup_screen/icon_with_label_button.dart';
 
-class SelectSigninSginupScreen extends StatelessWidget {
+class SelectSigninSignupScreen extends StatefulWidget {
+  @override
+  _SelectSigninSignupScreenState createState() =>
+      _SelectSigninSignupScreenState();
+}
+
+class _SelectSigninSignupScreenState extends State<SelectSigninSignupScreen> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   final _backgroundImagePath = "assets/images/background.png";
   final _facebookIconPath = "assets/images/facebook_logo.svg";
   final _appleIconPath = "assets/images/apple_logo.svg";
@@ -17,14 +29,82 @@ class SelectSigninSginupScreen extends StatelessWidget {
   final _appleLabel = "Appleでログイン";
   final _mailLabel = "メールアドレスで登録";
 
-  VoidCallback? _onPressFacebook() {
-    // Facebookのログイン処理
+  Future _onPressFacebook() async {
+    try {
+      // Trigger the sign-in flow
+      final LoginResult loginResult = await FacebookAuth.instance.login();
+
+      // Create a credential from the access token
+      final OAuthCredential facebookAuthCredential =
+          FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+      final User user =
+          (await _auth.signInWithCredential(facebookAuthCredential)).user!;
+      return user;
+    } catch (e) {
+      print('facebookログインエラー');
+      print(e);
+      return null;
+    }
   }
-  VoidCallback? _onPressGoogle() {
-    // Googleのログイン処理
+
+  Future _onPressGoogle() async {
+    try {
+      GoogleSignInAccount? googleCurrentUser = await _googleSignIn.signIn();
+      print(googleCurrentUser);
+      if (googleCurrentUser == null) return null;
+
+      // auth情報を得る
+      GoogleSignInAuthentication googleAuth =
+          await googleCurrentUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      final User user = (await _auth.signInWithCredential(credential)).user!;
+      print("signed in " + user.displayName.toString());
+
+      return user;
+    } catch (e) {
+      print('ログインエラーです');
+      print(e);
+      return null;
+    }
   }
-  VoidCallback? _onPressApple() {
+
+  Future _onPressApple() async {
     // Appleのログイン処理
+    print('apple');
+    return null;
+  }
+
+  Future signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      print('ログアウト成功');
+    } catch (e) {
+      print('ログアウトエラー');
+      print(e);
+    }
+  }
+
+  void checkLoginAuth() {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('ログアウト中');
+      } else {
+        print('ログイン中');
+        print(user);
+        print(user.displayName.toString() + 'でログインしています');
+        // TODO: ページ遷移
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    checkLoginAuth();
+    super.initState();
   }
 
   @override
@@ -109,7 +189,17 @@ class SelectSigninSginupScreen extends StatelessWidget {
                       },
                     )
                   ],
-                )
+                ),
+                ElevatedButton(
+                  child: const Text('ログアウト'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.grey,
+                    onPrimary: Colors.white,
+                  ),
+                  onPressed: () {
+                    signOut();
+                  },
+                ),
               ],
             ),
           ),
