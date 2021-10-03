b44880049c78be9a91a5dbb537842c2badf948f4
FujimuraKaito <kaito.bakuman@gmail.com>
2021-10-03 16:07:23 +0900

    [add] add comment class

---
+++ lib/models/Comment.dart
@@ -0,0 +1,12 @@
+import 'package:dish/models/TimelineUser.dart';
+
+class Comment {
+  final String id, content;
+  final User user;
+
+  Comment({
+    required this.id,
+    required this.content,
+    required this.user,
+  });
+}
