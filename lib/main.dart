8ddf9d742d6921e3aab4b9cd41173dac6e79f4ea
tomo <tmrtkm1227tksx@gmail.com>
2021-11-10 22:57:02 +0900

    [add]#54 Firestore library

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:dish/screens/select_signin_signup_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
