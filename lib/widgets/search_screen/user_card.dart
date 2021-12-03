f1cbd17bb1580a4bc8fd7ac9f1b7681f31159917
FujimuraKaito <kaito.bakuman@gmail.com>
2021-12-03 11:07:09 +0900

    [add] add user card

---
+++ lib/widgets/search_screen/user_card.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+import 'package:dish/models/User.dart';
+import 'package:dish/screens/profile_screen.dart';
+import 'package:dish/configs/constant_colors.dart';
+
+class UserCard extends StatelessWidget {
+  const UserCard({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 28,
+        vertical: 12,
+      ),
+      decoration: BoxDecoration(color: Color(0xFFFBFBFB)),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (_) => ProfileScreen(uid: user.uid!)),
+              );
+            },
+            child: Container(
+              height: 45,
+              width: 45,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: AppColor.kPinkColor,
+                  width: 2,
+                ),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(50)),
+                child: Image.network(
+                  user.iconImageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 12),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                user.userName,
+                style: TextStyle(
+                  color: AppColor.kPrimaryTextColor,
+                  fontSize: 12,
+                ),
+              ),
+              Text(
+                user.userId,
+                style: TextStyle(
+                  color: AppColor.kPrimaryTextColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
